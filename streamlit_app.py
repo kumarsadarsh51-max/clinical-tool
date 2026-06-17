84f6e1b8321c8e5fa9279c47080a1f2a19c40945
kumarsadarsh51-max <kumarsadarsh51@gmail.com>
2026-06-17 14:30:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -75,7 +75,7 @@ Test Type    : {cancer.upper()} RISK ASSESSMENT
 --------------------------------------------------
 Clinical Markers:
 """
-        for i, name in enumerate(info["names"]):
+    for i, name in enumerate(info["names"]):
             report_content += f"- {name}: {X_raw[i]}\n"
 
         report_content += f"""
